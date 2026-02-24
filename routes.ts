348ec9314745211ed89715fe0cc3639139550260
Hanshika Shanvi <hshanvi30@gmail.com>
2026-02-24 22:21:00 +0530

    Deployment

--- routes.ts
+++ routes.ts
@@ -12,4 +12,4 @@ export const authRoutes: string[] = [
 
 export const apiAuthPrefix: string = "/api/auth"
 
-export const DEFAULT_LOGIN_REDIRECT = "/";
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
